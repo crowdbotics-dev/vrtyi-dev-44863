d42d19aefe89bdf64a2d30f7901ca0611e28a6f5
Crowdbotics <engineering@crowdbotics.com>
2022-11-16 17:23:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vrtyi_dev_44863/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vrtyi_dev_44863;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vrtyi_dev_44863";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vrtyi-dev-44863",
+  url: "https://vrtyi-dev-44863.botics.co",
+  api: "https://vrtyi-dev-44863.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
